d7d2d0c6645a43a2540111a6f975a2e1948a9a6a
ultrasuperpingu <ultrapingu@gmail.com>
2023-07-19 22:06:16 +0200

    change version to 1.6

--- LibTinyPG/Templates/Java/TokenType.java
+++ LibTinyPG/Templates/Java/TokenType.java
@@ -1,5 +1,5 @@
 ﻿// Automatically generated from source file: <%SourceFilename%>
-// By TinyPG v1.5 available at https://github.com/ultrasuperpingu/TinyPG
+// By TinyPG v1.6 available at https://github.com/ultrasuperpingu/TinyPG
 
 package <%Namespace%>;
 
